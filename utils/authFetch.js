17fd785d39a271e6427e39fd07ec202835e62336
Paulo Tejada <paulotejada@MacBook-Air-de-Paulo.local>
2024-04-11 14:45:48 -0500

    Pandora Login Page complete

---
+++ utils/authFetch.js
@@ -0,0 +1,34 @@
+import { Auth, Token } from "@/api";
+
+export async function authFetch(url, params) {
+  const tokenCtrl = new Token();
+
+  const token = tokenCtrl.getToken();
+
+  const logout = () => {
+    tokenCtrl.removeToken();
+    window.location.replace("/login");
+  };
+
+  if (!token) {
+    logout();
+  } else {
+    if (tokenCtrl.hasExpired(token)) {
+      logout();
+    } else {
+      const paramsTemp = {
+        ...params,
+        headers: {
+          ...params?.headers,
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        return await fetch(url, paramsTemp);
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+}
