5c7ecee9c4909f1725e2b8a05379189d14ba9958
Paulo Tejada <paulotejada.123a@gmail.com>
2024-07-12 10:40:19 -0500

    solving issues in header

--- components/Investigations/index.js
+++ components/Investigations/index.js
@@ -118,7 +118,7 @@ export default function InvestigationsComponent() {
           ) {
             await getInvestigations();
           } else {
-            await filterInvestigations({
+            const investigations = await filterInvestigations({
               project: filters.project === "Todos" ? "" : filters.project,
               objectivePublic:
                 filters.objectivePublic === "Todos"
@@ -200,22 +200,14 @@ export default function InvestigationsComponent() {
         <div className="w-3/4 self-start">
           {investigations.length === 0 && (
             <div className="flex flex-col items-center justify-center h-96 text-gray-500 text-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-12"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-                />
-              </svg>
+              <Image
+                height={70}
+                width={70}
+                alt="not_found"
+                src="/investigations/not_found.svg"
+              />
 
-              <p className="text-xl">
+              <p className="text-md mt-2">
                 No se encontraron <br /> investigaciones
               </p>
             </div>
